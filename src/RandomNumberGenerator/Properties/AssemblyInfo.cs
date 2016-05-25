97b271aa3a701339989dae3f4febb472abe8b83f
CDillinger <collin.dillinger@gmail.com>
2016-05-25 18:38:53 -0400

    Changed AssemblyInfo to pull app version from App prooperty

--- src/RandomNumberGenerator/Properties/AssemblyInfo.cs
+++ src/RandomNumberGenerator/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using RandomNumberGenerator;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -23,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion(App.Version)]
+[assembly: AssemblyFileVersion(App.Version)]
 [assembly: ComVisible(false)]
